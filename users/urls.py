83005d902d2040f4a466c41136723479523ea98f
SamTheKorean <bumsuyi@gmail.com>
2024-06-25 03:13:23 +0900

    resolve : issue #17

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('user-groups/', UserGroupList.as_view(), name='user-group-list'),
     path('user-groups/<int:pk>/', UserGroupDetail.as_view(), name='user-group-detail'),
     path('user-groups/<int:pk>/task-containers/', TaskContainerListByUserGroup.as_view(), name='taskcontainer-list-by-user-group'),
+    path('users/<int:pk>/user-groups/', UserGroupList.as_view(), name='user-group-list')
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from users.models import UserGroup
 from task.models import TaskContainer
 from .serializers import UserGroupSerializer, TaskContainerSerializer
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserGroupList(generics.ListCreateAPIView):
     queryset = UserGroup.objects.all()
@@ -12,6 +16,14 @@ class UserGroupList(generics.ListCreateAPIView):
 class UserGroupDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserGroup.objects.all()
     serializer_class = UserGroupSerializer
+
+class UserGroupListView(generics.ListAPIView):
+    serializer_class = UserGroupSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs.get('pk')
+        user = get_object_or_404(User, id=user_id)
+        return user.user_groups.all()
 
 class TaskContainerListByUserGroup(APIView):
     def get(self, request, pk, format=None):
