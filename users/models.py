bb462f873ee5e9147d7a8d8cd8691b80a29d6384
kevin <hyunbin7303@gmail.com>
2024-06-30 11:43:57 -0400

    User setting model design #19

--- users/models.py
+++ users/models.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import AbstractUser
 class UserGroup(models.Model):
     group_name = models.CharField(max_length=100)
     group_type = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return f'{self.group_name} ({self.group_type})'
 
 class UserSetting(models.Model):
+    default_group_id = models.IntegerField(null=True)
     # Define any fields for UserSetting if needed
     pass
 
