77e77e8854b2dde738c231f96d60f83525b8b8ef
SamTheKorean <bumsuyi@gmail.com>
2024-07-07 15:43:06 +0900

    fix : clean up the files

--- users/apps.py
+++ users/apps.py
@@ -1,13 +1,6 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
-
-<<<<<<< HEAD
-# class UsersConfig(AppConfig):
-#     default_auto_field = "django.db.models.BigAutoField"
-#     name = "users"
-=======
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
->>>>>>> bbaa0bd (fix : add files that git didnt recognize)
