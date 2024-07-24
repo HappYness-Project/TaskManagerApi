2d734d9725d7a3ff23bc672a2a8d30e8b5bcdb38
SamTheKorean <bumsuyi@gmail.com>
2024-07-24 14:49:28 -0400

    feat : add first name and last name first and getting the user detail

--- users/serializers.py
+++ users/serializers.py
@@ -21,18 +21,20 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'user_setting', 'user_groups']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'user_setting', 'user_groups']
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
+            first_name=validated_data('first_name'),
+            last_name=validated_data('last_name'),
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
